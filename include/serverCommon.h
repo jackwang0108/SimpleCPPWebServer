48c2cee5a918fcada670eaf87391a1408b8921f9
jackWang <jack4shihong@gmail.com>
2024-01-31 16:07:46 +0800

    add docstring

--- include/serverCommon.h
+++ include/serverCommon.h
@@ -26,6 +26,13 @@ namespace idc {
 	 * @return 删除后的字符串
 	 */
 	char *ltrim(char *str, const char c = ' ');
+
+	/**
+	 * @brief 删除字符串str左侧的字符c
+	 * @param str 要删除的字符串
+     * @param c 要删除的字符
+	 * @return 删除后的字符串
+	 */
 	std::string &ltrim(std::string &str, const char c = ' ');
 
 	/**
@@ -35,6 +42,13 @@ namespace idc {
 	 * @return 删除后的字符串
 	 */
 	char *rtrim(char *str, const char c = ' ');
+
+	/**
+	 * @brief 删除字符串str右侧的字符c
+	 * @param str 要删除的字符串
+	 * @param c 要删除的字符
+	 * @return 删除后的字符串
+	 */
 	std::string &rtrim(std::string &str, const char c = ' ');
 
 	/**
@@ -44,6 +58,13 @@ namespace idc {
 	 * @return 删除后的字符串
 	 */
 	char *trim(char *str, const char c = ' ');
+
+	/**
+	 * @brief 删除字符串str左右两侧的字符c
+	 * @param str 要删除的字符串
+	 * @param c 要删除的字符
+	 * @return 删除后的字符串
+	 */
 	std::string &trim(std::string &str, const char c = ' ');
 
 };// namespace idc
