81b952e0d1e202f75ce4de0c2e0a3fcb69658494
jackWang <jack4shihong@gmail.com>
2024-02-02 20:10:29 +0800

    add timeToStr, getCurrTime, strToTime, strTimeCalculation

--- src/serverTime.cpp
+++ src/serverTime.cpp
@@ -1,1 +1,82 @@
 #include "serverCommon.h"
+
+using std::string;
+using std::time_t;
+
+
+string &idc::timeToStr(const time_t time, string &out, const string &fmt) {
+	tm timeTm;
+	localtime_r(&time, &timeTm);
+	std::stringstream ss;
+	ss << std::put_time(&timeTm, fmt.c_str());
+	out = ss.str();
+	return out;
+}
+
+string idc::timeToStr(const time_t time, const string &fmt) {
+	string result;
+	idc::timeToStr(time, result, fmt);
+	return result;
+}
+
+char *idc::timeToStr(const time_t time, char *out, const string &fmt) {
+	if (out == nullptr)
+		return nullptr;
+	string result;
+	idc::timeToStr(time, result, fmt);
+	result.copy(out, result.length(), 0);
+	return out;
+}
+
+time_t idc::strToTime(const string &strTime, const std::string &fmt) {
+	// 这里一定要0初始化, 否则mktime可能会因为未填的遗留字段导致返回-1
+	tm t{};
+	std::istringstream iss(strTime);
+	iss >> std::get_time(&t, fmt.c_str());
+	if (iss.fail())
+		return -1;
+	return std::mktime(&t);
+}
+
+string &idc::getCurrTime(string &out, const int timeInterval, const string &fmt) {
+	time_t now = std::time(nullptr) + timeInterval;
+	idc::timeToStr(now, out, fmt);
+	return out;
+}
+
+string idc::getCurrTime(const int timeInterval, const string &fmt) {
+	time_t now = std::time(nullptr) + timeInterval;
+	return idc::timeToStr(now, fmt);
+}
+
+char *idc::getCurrTime(char *out, const int timeInterval, const string &fmt) {
+	time_t now = std::time(nullptr) + timeInterval;
+	idc::timeToStr(now, out, fmt);
+	return out;
+}
+
+bool idc::strTimeCalculation(const string &origin, string &out, int timeInterval, const string &fmt) {
+	time_t originTime = idc::strToTime(origin, fmt);
+	if (originTime == -1)
+		return false;
+	time_t outTime = originTime + timeInterval;
+	idc::timeToStr(outTime, out, fmt);
+	return true;
+}
+
+bool idc::strTimeCalculation(const string &origin, char *out, int timeInterval, const string &fmt) {
+	time_t originTime = idc::strToTime(origin, fmt);
+	if (originTime == -1)
+		return false;
+	time_t outTime = originTime + timeInterval;
+	idc::timeToStr(outTime, out, fmt);
+	return true;
+}
+
+string idc::strTimeCalculation(const string &origin, int timeInterval, const string &fmt) {
+	time_t originTime = idc::strToTime(origin, fmt);
+	if (originTime == -1)
+		return "";
+	time_t outTime = originTime + timeInterval;
+	return idc::timeToStr(outTime, fmt);
+}
--- test/testServerTime.cpp
+++ test/testServerTime.cpp
@@ -8,7 +8,60 @@ int main(int argc, char *argv[]) {
 	return RUN_ALL_TESTS();
 }
 
+using std::cout;
+using std::string;
 
-TEST(TestSuiteName, t1) {
-	EXPECT_EQ(1, 1);
+std::time_t now = 1706861110;
+
+TEST(TestSuiteName, timeToStr1) {
+	string nowStr = idc::timeToStr(now);
+
+	::testing::internal::CaptureStdout();
+	cout.imbue(std::locale("en_US.utf8"));
+	cout << nowStr;
+	string output = ::testing::internal::GetCapturedStdout();
+
+	string expected = "2024-02-02 16:05:10";
+	EXPECT_EQ(output, expected);
+}
+
+TEST(TestSuiteName, timeToStr2) {
+	chrono::time_point nowTP = chrono::system_clock::from_time_t(now);
+
+	string nowStr = idc::timeToStr(nowTP);
+	::testing::internal::CaptureStdout();
+	cout.imbue(std::locale("en_US.utf8"));
+	cout << nowStr;
+	string output = ::testing::internal::GetCapturedStdout();
+
+	string expected = "2024-02-02 16:05:10";
+	EXPECT_EQ(output, expected);
+}
+
+TEST(TestSuiteName, getCurrTime) {
+	string result1, result2 = idc::getCurrTime();
+	idc::getCurrTime(result1);
+	EXPECT_EQ(result1, result2);
+}
+
+TEST(TestSuiteName, strToTime) {
+	string timeStr = idc::timeToStr(now);
+	time_t time = idc::strToTime(timeStr);
+	EXPECT_EQ(time, now);
+}
+
+TEST(TestSuiteName, strTimeCalculation1) {
+	time_t after = now + 10;
+	string nowStr = idc::timeToStr(now);
+	string afterStr = idc::timeToStr(after);
+	string calculated = idc::strTimeCalculation(nowStr, 10);
+	EXPECT_EQ(afterStr, calculated);
+}
+
+TEST(TestSuiteName, strTimeCalculation2) {
+	time_t after = now + 60;
+	string nowStr = idc::timeToStr(now);
+	string afterStr = idc::timeToStr(after);
+	string calculated = idc::strTimeCalculation(nowStr, 1min);
+	EXPECT_EQ(afterStr, calculated);
 }
